00cac7245d9a8b0819a47c111f94743fe115829d
nazzaladiy <zalnahanifa28@gmail.com>
2022-03-05 21:46:17 +0700

    Praktikum Modul 3

--- src/index.js
+++ src/index.js
@@ -1,30 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 //import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
+//import HelloComponent from './components/HelloComponent';
+import Image from './Image';
+import List from './List';
+import Test from './Test';
+//import { ReactComponent } from '*.svg';
 
-const Hello = () => {
+//const Hello = () => {
 
-  return <p>hello</p>
-}
+  //return <p>hello</p>
+//}
 //import reportWebVitals from './reportWebVitals';
 
-//ReactDOM.render(
   //<React.StrictMode>
     //<App />
   //</React.StrictMode>,
   //document.getElementById('root')
 //);
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<List />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Test />, document.getElementById('root'));
+//ReactDOM.render(<Hello />, document.getElementById('root'));
 
-//ReactDOM.render(<p/>, document.getElementById('root'));
+//ReactDOM.render(<App/>, document.getElementById('root'));
+//const HelloComponent = () => {
+  //return <p> HelloComponent </p>
+//}
 
+  //ReactDOM.render(<HelloComponent />, document.getElementById('root'));
+//class StatefullComponent extends React.Component{
+  //render () {
+    //return <p>StatefullComponent</p>
+  //}
+//}
+
+//ReactDOM.render(<StatefullComponent />, document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
-
 reportWebVitals();
